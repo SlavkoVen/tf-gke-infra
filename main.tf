f27524ef65476163abe3a1583115b521086f60ac
SlavkoVen <46169342+SlavkoVen@users.noreply.github.com>
2025-09-20 14:05:43 +0000

    Initial commit: Terraform GKE infra

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "gcs" {
+    bucket = "tf-gke-infra-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = var.GOOGLE_PROJECT
+  region  = var.GOOGLE_REGION
+}
+
+module "gke_cluster" {
+  source         = "github.com/SlavkoVen/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = var.GKE_NUM_NODES
+}
